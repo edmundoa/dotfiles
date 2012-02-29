f6d62c0ba3f7e280f136449fa13467fa73a221ef
Edmundo Alvarez <edmundo.alvarez@xing.com>
2012-02-29 10:30:33 +0100

    Updated install.sh to also clone NERDTree.

--- install.sh
+++ install.sh
@@ -1,6 +1,17 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+NERDTREE_REPO="https://github.com/scrooloose/nerdtree.git"
+NERDTREE_PATH="$HOME/.dotfiles/vim/bundle/nerdtree"
+
 for file in $(ls $HOME/.dotfiles/); do
     if [ ! -e "$HOME/.$file" ]; then
         ln -sf $HOME/.dotfiles/$file $HOME/.$file
     fi
 done
+
+if [ "$(ls -A $NERDTREE_PATH)" ]; then
+  echo "NERDTree seems to be already installed"
+else
+  echo "Cloning NERDTree for vim..."
+  git clone $NERDTREE_REPO $NERDTREE_PATH
+fi
