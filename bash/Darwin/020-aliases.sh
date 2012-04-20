e8ab4e1425082f09e1a0379f5118473b600a93a6
Edmundo Alvarez <edmundo.alvarez@xing.com>
2012-04-20 14:45:55 +0200

    This aliases are no longer needed using lunchy

--- bash/Darwin/020-aliases.sh
+++ bash/Darwin/020-aliases.sh
@@ -1,4 +1,2 @@
 alias ls="ls -G"
 alias textmate="open -a /Applications/TextMate.app/Contents/MacOS/TextMate"
-alias launch-mongo="mongod run --config /usr/local/Cellar/mongodb/2.0.2-x86_64/mongod.conf"
-alias launch-es="elasticsearch -f -D es.config=/usr/local/Cellar/elasticsearch/0.18.7/config/elasticsearch.yml"
