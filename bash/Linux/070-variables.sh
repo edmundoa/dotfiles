fb7f91c033ea57708057f8f2d0b184f418aaa8e1
Edmundo Alvarez <e.alvarezj@gmail.com>
2012-05-10 21:25:15 +0200

    Add JAVA_HOME variable

--- bash/Linux/070-variables.sh
+++ bash/Linux/070-variables.sh
@@ -4,3 +4,5 @@ export PATH=$PATH:/home/edmundoa/Bin/android-sdk-linux_x86/platform-tools
 
 export DEBEMAIL=e.alvarezj@gmail.com
 export DEBFULLNAME="Edmundo Alvarez Jimenez"
+
+export JAVA_HOME=/usr/lib/jvm/java-6-openjdk-amd64/jre/bin
