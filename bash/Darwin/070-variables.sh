35b4fcf2949a93d707403100ffb867f5a4a2dc79
Edmundo Alvarez <edmundo.alvarez@xing.com>
2012-12-21 11:26:04 +0100

    Changes in path

--- bash/Darwin/070-variables.sh
+++ bash/Darwin/070-variables.sh
@@ -1,7 +1,5 @@
+export PATH=/usr/local/bin:$PATH
 export PATH=$PATH:/usr/local/mysql-5.0.91-osx10.5-x86_64/bin
-export PATH=$PATH:/opt/local/bin
-export PATH=$PATH:/opt/local/sbin
-export PATH=$PATH:/usr/local/bin
 
 export CLICOLOR=1 # for terminal colors
 export GREP_OPTIONS='--color=auto'
