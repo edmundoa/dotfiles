bcd3f487b756b70cb645dfb31f668f722fce216b
Edmundo Alvarez <edmundo.alvarez@xing.com>
2012-12-21 11:26:45 +0100

    Add go path

--- bash/Darwin/070-variables.sh
+++ bash/Darwin/070-variables.sh
@@ -1,5 +1,6 @@
 export PATH=/usr/local/bin:$PATH
 export PATH=$PATH:/usr/local/mysql-5.0.91-osx10.5-x86_64/bin
+export PATH=$PATH:/usr/local/go/bin
 
 export CLICOLOR=1 # for terminal colors
 export GREP_OPTIONS='--color=auto'
