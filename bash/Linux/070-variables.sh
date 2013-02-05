4a4f8991578dc73e65b334f474357612f797cd0b
Edmundo Alvarez <e.alvarezj@gmail.com>
2013-02-05 19:41:05 +0100

    Add gPodder env variables

--- bash/Linux/070-variables.sh
+++ bash/Linux/070-variables.sh
@@ -6,3 +6,6 @@ export DEBEMAIL=e.alvarezj@gmail.com
 export DEBFULLNAME="Edmundo Alvarez Jimenez"
 
 export JAVA_HOME=/usr/lib/jvm/java-6-openjdk-amd64/jre
+
+export GPODDER_HOME=$HOME/.gPodder
+export GPODDER_DOWNLOAD_DIR=$HOME/Podcasts
