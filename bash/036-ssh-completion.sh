c172da4c4e7881c9444ed9c70414e534addf7985
Edmundo Alvarez <edmundo.alvarez@xing.com>
2013-04-30 15:03:36 +0200

    Add ssh completion for bash

---
+++ bash/036-ssh-completion.sh
@@ -0,0 +1,20 @@
+_complete_ssh_hosts ()
+{
+        COMPREPLY=()
+        cur="${COMP_WORDS[COMP_CWORD]}"
+        comp_ssh_hosts=`cat ~/.ssh/known_hosts | \
+                        cut -f 1 -d ' ' | \
+                        sed -e s/,.*//g | \
+                        grep -v ^# | \
+                        uniq | \
+                        grep -v "\[" ;
+                if [ -f ~/.ssh/config ]; then
+                        cat ~/.ssh/config | \
+                                grep "^Host " | \
+                                awk '{print $2}'
+                fi
+                `
+        COMPREPLY=( $(compgen -W "${comp_ssh_hosts}" -- $cur))
+        return 0
+}
+complete -F _complete_ssh_hosts ssh
