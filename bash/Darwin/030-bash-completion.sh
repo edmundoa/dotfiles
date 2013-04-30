953c827b7cd8a4c16b53bacdc1141fe61d3d71a9
Edmundo Alvarez <edmundo.alvarez@xing.com>
2013-04-30 15:03:22 +0200

    Cleanup macos completion

--- bash/Darwin/030-bash-completion.sh
+++ bash/Darwin/030-bash-completion.sh
@@ -1,13 +1,2 @@
 # Completion for sudo command
 complete -c -f command sudo
-
-# Git completion script
-if [ -f /opt/local/etc/bash_completion.d/git ]; then
-  . /opt/local/etc/bash_completion.d/git
-fi
-
-complete -o default -o nospace -W "$(grep host < ~/.ssh/config | cut -d ' ' -f2)" scp ssh
-
-# Rake completion script
-# complete -C /opt/local/etc/bash_completion.d/rake-completion.rb -o default rake
-
